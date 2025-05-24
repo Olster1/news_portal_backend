eeb43f218dfd98efc0c44a5385b1532338beca02
Olster1 <oliver_marsh@live.com.au>
2025-05-25 08:41:46 +1200

    made the tests available and created a seperate dockerfile in the tests folder. We would run this test build as part of the deployment pipeline.

--- tests/NewsControllerTest.cs
+++ tests/NewsControllerTest.cs
@@ -1,49 +1,46 @@
-/*NOTE: 
-This is my tests 
-*/
-// using Xunit;
-// using Microsoft.AspNetCore.Mvc;
-// using System.Threading.Tasks;
-// using MyApp.Controllers.News;
-// using MyApp.Models.NewsArticle;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using MyApp.Controllers.News;
+using MyApp.Models.NewsArticle;
 
-// public class NewsControllerTests
-// {
-//     [Fact]
-//     public async Task Get_WithQuery_ReturnsOkWithArticles()
-//     {
-//         // Arrange
-//         var fakeService = new FakeNewsService();
-//         var controller = new NewsController(fakeService);
-//         var testQuery = "testing";
+public class NewsControllerTests
+{
+    [Fact]
+    public async Task Get_WithQuery_ReturnsOkWithArticles()
+    {
+        // Arrange
+        var fakeService = new FakeNewsService();
+        var controller = new NewsController(fakeService);
+        var testQuery = "testing";
 
-//         // Act
-//         var result = await controller.Get(testQuery);
+        // Act
+        var result = await controller.Get(testQuery);
 
-//         // Assert
-//         var okResult = Assert.IsType<OkObjectResult>(result);
-//         var articles = Assert.IsAssignableFrom<List<NewsArticle>>(okResult.Value);
-//         Assert.Single(articles);
-//         Assert.Contains(testQuery, articles[0].Title);
-//         Assert.Equal(testQuery, fakeService.LastSearchQuery);
-//     }
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var articles = Assert.IsAssignableFrom<List<NewsArticle>>(okResult.Value);
+        Assert.Single(articles);
+        Assert.Contains(testQuery, articles[0].Title);
+        Assert.Equal(testQuery, fakeService.LastSearchQuery);
+    }
 
-//     [Fact]
-//     public async Task GetList_WithN_ReturnsOkWithArticles()
-//     {
-//         // Arrange
-//         var fakeService = new FakeNewsService();
-//         var controller = new NewsController(fakeService);
-//         int n = 5;
+    [Fact]
+    public async Task GetList_WithN_ReturnsOkWithArticles()
+    {
+        // Arrange
+        var fakeService = new FakeNewsService();
+        var controller = new NewsController(fakeService);
+        int n = 5;
 
-//         // Act
-//         var result = await controller.GetList(n);
+        // Act
+        var result = await controller.GetList(n);
 
-//         // Assert
-//         var okResult = Assert.IsType<OkObjectResult>(result);
-//         var articles = Assert.IsAssignableFrom<List<NewsArticle>>(okResult.Value);
-//         Assert.Single(articles);
-//         Assert.Contains(n.ToString(), articles[0].Title);
-//         Assert.Equal(n, fakeService.LastLatestNewsCount);
-//     }
-// }
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var articles = Assert.IsAssignableFrom<List<NewsArticle>>(okResult.Value);
+        Assert.Single(articles);
+        Assert.Contains(n.ToString(), articles[0].Title);
+        Assert.Equal(n, fakeService.LastLatestNewsCount);
+    }
+}
